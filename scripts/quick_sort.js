5ef5fc3590d8e8c31226cb0cf9fad0dbcfa50044
RajatThakur-27 <142075008+RajatThakur-27@users.noreply.github.com>
2025-03-18 14:41:37 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,9 +1,3 @@
-/*
-    *****************
-    DONE BY:-  Aniket
-    
-    *****************
-*/
 
 function Quick()
 {
@@ -79,10 +73,3 @@ function quick_sort (start, end )
         quick_sort (piv_pos +1, end) ;//sorts the right side of pivot.
     }
  }
-
-/*
-    *****************
-    DONE BY:-  Aniket
-    
-    *****************
-*/
